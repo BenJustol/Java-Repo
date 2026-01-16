833eb957546ce377553110488e46dad5f0f93310
Ben Justol <benedictjustol10@gmail.com>
2026-01-16 08:20:03 -0500

    STARTING FRESH'

--- Main.java
+++ Main.java
@@ -1,21 +1,7 @@
-import java.util.List;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("HELLO FROM MAIN");
-
-        LinkedList<Integer> numbers = new LinkedList<Integer>();
-
-        numbers.add(10);
-        numbers.add(11);
-        numbers.add(12);
-
-        System.out.println(numbers);
-        System.out.println(numbers.size());
-
-        // HELLO GITHUB //
-
-        // DOING WORK ....// 
+    public static void Main(String[] args) {
+        System.out.println("hello");
     }
 }
